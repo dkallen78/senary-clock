2a7c91cd662395b460fb1924cbfc35f5609a4ac5
Daniel <32464403+dkallen78@users.noreply.github.com>
2020-07-26 03:35:10 -0500

    refactored a bit
    now with more comments!

--- senaryClockSVG.js
+++ senaryClockSVG.js
@@ -1,71 +1,82 @@
 const getRandomNumber = function(floor, ceiling) {
   //----------------------------------------------------//
-  //Gets random number for the math problems            //
-  //where floor is the lowest number possible and       //
-  //ceiling is the highest number possible              //
+  //Gets random number within a range of numbers        //
+  //integer-> floor: the lowest possible number         //
+  //integer-> ceiling: the highest possible number      //
   //----------------------------------------------------//
 
   let range = (ceiling - floor) + 1;
   return Math.floor((Math.random() * range) + floor);
 }
 
-function makeCircle() {
+function makeSVG(type) {
+  let svg = document.createElementNS("http://www.w3.org/2000/svg", type);
+  if (arguments.length > 0) {svg.id = arguments[1]};
+  return svg;
+}
+
+function makeCircle(parent) {
   //----------------------------------------------------//
   //Makes the circle SVG element for the outer face     //
-  //of the clock                                        //
+  //  of the clock                                      //
+  //element-> parent: the <svg> element into which      //
+  //  the circle is inserted                            //
   //----------------------------------------------------//
 
-  let circle = document.createElementNS(svgNS, "circle");
+  let circle = makeSVG("circle", "rim");
   circle.classList.add("circle");
-  circle.id = "rim";
 
   circle.style.stroke = "aqua";
   circle.style.strokeWidth = 3;
   circle.style.fill = "none";
 
+  parent.appendChild(circle);
+
   return circle;
 }
 
 function makeCenter(parent) {
-  let circle = document.createElementNS(svgNS, "circle");
+  //----------------------------------------------------//
+  //Makes the center hub of the clock face              //
+  //element-> parent: the <svg> element into which      //
+  //  the center is inserted                            //
+  //----------------------------------------------------//
+
+  let circle = makeSVG("circle", "center");
   circle.classList.add("circle");
-  circle.id = "center";
-
   circle.style.fill = "aqua";
-
   parent.appendChild(circle);
 }
 
 function makeEllipses(parent, number) {
-
-  let defs = document.createElementNS(svgNS, "defs");
-
-  let gradient = document.createElementNS(svgNS, "radialGradient");
-  gradient.id = "ellipseGradient";
-
-  addStop(gradient, "0%", "violet");
-  addStop(gradient, "50%", "orchid");
-  addStop(gradient, "100%", "fuchsia");
-
-  defs.appendChild(gradient);
-  parent.appendChild(defs);
+  //----------------------------------------------------//
+  //Makes the ellipses that point to the hours          //
+  //element-> parent: the <svg> element into which      //
+  //  the ellipses will be inserted                     //
+  //integer-> number: the number of ellipses to make    //
+  //----------------------------------------------------//
 
   let angle = 0;
   let change = 360 / number;
 
   for (let i = 0; i < number; i++) {
-    let ellipse = document.createElementNS(svgNS, "ellipse");
+    let ellipse = makeSVG("ellipse");
     ellipse.classList.add("ellipse");
     ellipse.style.transform = "rotate(" + angle + "deg)";
     parent.appendChild(ellipse);
 
     angle += change;
   }
-
-
 }
 
 function makeNumbers(parent, number) {
+  //----------------------------------------------------//
+  //Makes the numbers that surround the clock face      //
+  //element-> parent: the <svg> element into which      //
+  //  the numbers will be inserted                      //
+  //integer-> number: the number of numbers to make     //
+  //----------------------------------------------------//
+
   let angle = 0;
   let change = 360 / number;
 
@@ -73,8 +84,8 @@ function makeNumbers(parent, number) {
     angle += change;
 
     let digit = i + 1;
-    let numero = document.createElementNS(svgNS, "text");
-    numero.innerHTML = digit.toString(6).padStart(2, 0);
+    let numero = makeSVG("text");
+    numero.textContent = digit.toString(6).padStart(2, 0);
     numero.classList.add("numbers");
     numero.style.transform = "rotate(" + angle + "deg) translate(41vh, 8vh)";
     parent.appendChild(numero);
@@ -88,41 +99,45 @@ function makeNumbers(parent, number) {
 }
 
 function makeHand(parent, id) {
-  let hand = document.createElementNS(svgNS, "rect");
-  hand.id = id;
+  //----------------------------------------------------//
+  //Makes the hands of the clock                        //
+  //element-> parent: the <svg> element into which      //
+  //  the hand will be inserted                         //
+  //string-> id: the id of the hand                     //
+  //----------------------------------------------------//
+
+  let hand = makeSVG("rect", id);
   hand.classList.add("hands");
-
-  let defs = document.createElementNS(svgNS, "defs");
-  let gradient = document.createElementNS(svgNS, "linearGradient");
-  gradient.id = "handGradient";
-  gradient.setAttribute("gradientTransform", "rotate(90)");
-
-  /*addStop(gradient, "0%", "dodgerBlue");
-  addStop(gradient, "100%", "aqua");*/
-
-  addStop(gradient, "0%", "orangered");
-  addStop(gradient, "100%", "gold");
-
-  defs.appendChild(gradient);
-  parent.appendChild(defs);
   parent.appendChild(hand);
 
-  hand.style.fill = "url('#handGradient')";
   return hand;
 }
 
 function makeHandShadow(parent, id) {
-  let hand = document.createElementNS(svgNS, "rect");
-  hand.id = id;
-  hand.classList.add("handShadows");
+  //----------------------------------------------------//
+  //Makes the shadows of the hands of the clock         //
+  //element-> parent: the <svg> element into which      //
+  //  the hand will be inserted                         //
+  //string-> id: the id of the hand                     //
+  //----------------------------------------------------//
 
+  let hand = makeSVG("rect", id);
+  hand.classList.add("handShadows");
   parent.appendChild(hand);
 
   return hand;
 }
 
 function addStop(parent, offset, color) {
-  let stop = document.createElementNS(svgNS, "stop");
+  //----------------------------------------------------//
+  //adds gradient <stop> elements into gradient elements//
+  //element-> parent: the gradient element into which   //
+  //  the <stop> will be inserted                       //
+  //string-> offset: the offset of the stop             //
+  //string-> color: the color of the gradient stop      //
+  //----------------------------------------------------//
+
+  let stop = makeSVG("stop");
   stop.setAttribute("offset", offset);
   stop.setAttribute("stop-color", color);
   parent.appendChild(stop);
@@ -140,10 +155,16 @@ function flicker() {
 
   setTimeout(function() {
     makeNumbers(svg, 24);
+    working = false;
   }, getRandomNumber(0, 100));
 }
 
 function illuminateHour(hour) {
+  //----------------------------------------------------//
+  //Adds an illumination effect to the current hour     //
+  //integer-> hour: the current hour from 0-23          //
+  //----------------------------------------------------//
+
   let newHour = ((hour + 24) - 1) % 24;
   let oldHour = ((hour + 24) - 2) % 24;
   let numbers = document.getElementsByClassName("numbers");
@@ -158,27 +179,51 @@ let clock = document.getElementById("clock");
 //
 //Makes the big SVG element to put in
 //the HTML document
-let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+let svg = makeSVG("svg", "svgClock");
 let svgNS = svg.namespaceURI;
-svg.id = "svgClock";
 
-let circle = makeCircle();
-
-svg.appendChild(circle);
-
+//
+//Sets up the face of the clock
+makeCircle(svg);
 makeEllipses(svg, 24);
 makeNumbers(svg, 24);
 
-let defs = document.createElementNS(svgNS, "defs");
-svg.appendChild(defs);
-let filter = document.createElementNS(svgNS, "filter");
-defs.appendChild(filter);
-filter.id = "blur";
-let blur = document.createElementNS(svgNS, "feGaussianBlur");
-filter.appendChild(blur);
-blur.setAttribute("in", "SourceGraphic");
-blur.setAttribute("stdDeviation", "2");
+//
+//This is the radial gradient for the ellipses
+let ellipseDef = makeSVG("defs");
+  let ellipseGrad = makeSVG("radialGradient", "ellipseGradient");
+    addStop(ellipseGrad, "0%", "violet");
+    addStop(ellipseGrad, "50%", "orchid");
+    addStop(ellipseGrad, "100%", "fuchsia");
+ellipseDef.appendChild(ellipseGrad);
+svg.appendChild(ellipseDef);
 
+//
+//This is the linear gradient for the hands
+let hands = makeSVG("defs");
+  let handGrad = makeSVG("linearGradient", "handGradient");
+  handGrad.setAttribute("gradientTransform", "rotate(90)");
+    addStop(handGrad, "0%", "orangered");
+    addStop(handGrad, "100%", "gold");
+hands.appendChild(handGrad);
+svg.appendChild(hands);
+
+/*addStop(gradient, "0%", "dodgerBlue");
+addStop(gradient, "100%", "aqua");*/
+
+//
+//This is the shadow filter for the hands
+let handShadows = makeSVG("defs");
+  let filter = makeSVG("filter", "blur");
+    let blur = makeSVG("feGaussianBlur");
+    blur.setAttribute("in", "SourceGraphic");
+    blur.setAttribute("stdDeviation", "2");
+  filter.appendChild(blur);
+handShadows.appendChild(filter);
+svg.appendChild(handShadows);
+
+//
+//Puts the three hands and their shadows on the SVG
 let sHandShadow = makeHandShadow(svg, "sHandShadow");
 let sHand = makeHand(svg, "sHand");
 
@@ -188,52 +233,66 @@ let mHand = makeHand(svg, "mHand");
 let hHandShadow = makeHandShadow(svg, "hHandShadow");
 let hHand = makeHand(svg, "hHand");
 
+//
+//Puts the center onto the face and puts
+//everything onto the page
 makeCenter(svg);
-
 clock.appendChild(svg);
 
 let numbers = document.getElementsByClassName("numbers");
-let digitalTime = document.createElementNS(svgNS, "text");
+let digitalTime = makeSVG("text", "digitalTime");
 digitalTime.setAttribute("textLength", "50%");
 svg.appendChild(digitalTime);
 
 //
 //Will update the clock face at 100Hz
 let hourCheck = null;
-
+let working = false;
 let refreshInterval = setInterval(function() {
-  if (getRandomNumber(0, 200) === 1) {
+  //
+  //Determines if the numbers will "flicker"
+  //this cycle
+  if ((getRandomNumber(0, 200) === 1) && !working) {
+    working = true;
     flicker();
   }
 
   let time = new Date();
 
+  //
+  //Does the work of moving the second hand
   let msSeconds = (time.getSeconds() * 1000) + time.getMilliseconds();
   let sAngle = msSeconds * 0.006;
   sHand.style.transform = "rotate(" + sAngle + "deg)";
   sHandShadow.style.transform = "rotate(" + sAngle + "deg)";
 
+  //
+  //Does the work of moving the minute hand
   let msMinutes = (time.getMinutes() * 60000) + msSeconds;
   let mAngle = msMinutes * 0.0001;
   mHand.style.transform = "rotate(" + mAngle + "deg)";
   mHandShadow.style.transform = "rotate(" + mAngle + "deg)";
 
+  //
+  //Does the work of moving the hour hand
   let msHours = (time.getHours() * 3600000) + msMinutes;
-  let hAngle = msHours * 0.00000833;
+  let hAngle = msHours * 0.000004166;
   hHand.style.transform = "rotate(" + hAngle + "deg)";
   hHandShadow.style.transform = "rotate(" + hAngle + "deg)";
 
+  //
+  //Gets and formats the numbers for the "digital" display
   let hour = time.getHours().toString(6).padStart(2, 0);
   let minute = time.getMinutes().toString(6).padStart(3, 0);
   let second = time.getSeconds().toString(6).padStart(3, 0);
   let timeString = hour + ":" + minute + ":" + second;
 
-  digitalTime.innerHTML = timeString;
-  digitalTime.id = "digitalTime";
+  digitalTime.textContent = timeString;
 
+  //
+  //Checks to see if the current hour is "illuminated"
   if (hour !== hourCheck) {
     hourCheck = hour;
     illuminateHour(time.getHours());
   }
-
 }, 10);
